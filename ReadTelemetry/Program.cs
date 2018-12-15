d6a014cd9d84c327046e8a09feadcb734f0d92cc
Francois Robbertze <frobbertze@gmail.com>
2018-12-15 09:50:37 +0200

    added cpu and ram perfromance counters

--- ReadTelemetry/Program.cs
+++ ReadTelemetry/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
+
 
 namespace ReadTelemetry
 {
@@ -10,6 +12,23 @@ namespace ReadTelemetry
     {
         static void Main(string[] args)
         {
+            long count = 0;
+            while (count < 90000000000)
+            {
+
+
+                var cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+                var ramCounter = new PerformanceCounter("Memory", "Available MBytes");
+                Console.Write(cpuCounter.NextValue() + "%");
+                Console.WriteLine();
+                Console.Write(ramCounter.NextValue() + "MB");
+                System.Threading.Thread.Sleep(3000);
+                Console.Clear();
+                count++;
+            }
+
+            Console.ReadLine();
+
         }
     }
 }
